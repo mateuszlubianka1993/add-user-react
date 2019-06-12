1ffee95e83551c660e1d61f0c9fda0ffe2e2cc68
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-06-12 11:45:53 +0200

    add form to AddUserForm component

--- src/components/AddUserForm.js
+++ src/components/AddUserForm.js
@@ -3,7 +3,26 @@ import React from 'react';
 class AddUserForm extends React.Component {
     render() {
         return(
-            <div>AddUserForm</div>
+            <div className="add-user-form-container">
+                <form className="add-user-form ui form">
+                    <div class="field">
+                        <label>Nickname</label>
+                        <input type="text" name="nick" placeholder="Nickname" required />
+                    </div>
+                    <div class="field">
+                        <label>E-mail</label>
+                        <input type="email" placeholder="your@email.com" required/>
+                    </div>
+                    <div class="field">
+                        <label>IP Address</label>
+                        <input type="text" name="ip-address" placeholder="xxx.xxx.xxx.xx" pattern="\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" required/>
+                    </div>
+                    <button class="positive ui button" type="submit">Add User</button>
+                </form>
+                <div className="delete-list-container">
+                    <button class="negative ui button">Remove List</button>
+                </div>
+            </div>
         );
     };
 };
